e934f62efb4fa375b45bc018d2adc222e55d6e55
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-07-27 02:13:50 +0530

    Revert "model removed"
    This reverts commit 4a7cea8494d60a58435799aa45546e492bd82fc4.

--- PropertyAnalyzer/install.sh
+++ PropertyAnalyzer/install.sh
@@ -7,7 +7,6 @@ pip3 install -r requirements.txt
 echo '----------------------------- Dependencies successfully installed --------------------------'
 echo '---------------------------- Running migrations --------------------------------------------'
 python3 manage.py makemigrations
-python3 manage.py migrate
 echo '----------------------------Completed migrations--------------------------------------------'
 echo '-------Installation completed succcessfully and server is about to start--------------------'
 echo '----------------------------starting server-------------------------------------------------'
